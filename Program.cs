ac2d969f6df2a8c4ef89a0f2d4562270f7704eec
Egor Antropov <antropov-zhora@mail.ru>
2022-08-23 21:20:44 +0300

    ddddddd

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите элементы (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int element = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        element++;
+    }
+}
+
+Console.WriteLine("Кол-во элементов > 0:" + element);
+
+
